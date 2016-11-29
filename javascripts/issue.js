a9793bfecf98b2b8f749f644fb04f95f8db18d73
fabcooldog <fabcooldog@users.noreply.github.com>
2016-11-29 11:43:54 +0000

    Update issue.js

--- javascripts/issue.js
+++ javascripts/issue.js
@@ -27,7 +27,7 @@ function MakeIssue(firstname, lastname, topic, body) {
 	    body: body
 	}));
      
-	document.getElementById("output").innerHTML = "<strong style="text-align: center;">Issue Submitted</strong>";
+	document.getElementById("output").innerHTML = "<h3><strong">Issue Submitted</strong></h3>";
 	document.getElementById("submit_issue").reset();
     });
 }
